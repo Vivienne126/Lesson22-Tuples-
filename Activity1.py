91150b72e89ac88f6ca2aa198ce40df38e380dae
Vivienne126 <vivienne.singh@cns.ac.in>
2025-03-11 21:48:17 +0530

    New21

---
+++ Activity1.py
@@ -0,0 +1,13 @@
+tuples1=("hello" , 2.3 , 2 , False)
+print(tuples1)
+
+tuple2=(1,12,3,4)
+print(tuple2)
+
+tuple2=tuple2+(9,)
+print(tuple2)
+
+tuple3=(9,9,9,9,9,1,2,3,4)
+print(tuple3.count(9))
+
+print(tuple2[1:4])
